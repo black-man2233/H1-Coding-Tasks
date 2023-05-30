cbbd20bdf119db9030bdb37dff3d042fcea19e9c
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-05-30 09:45:33 +0200

    reset

--- OOP/ObjectOrientede/CodeTesting/Program.cs
+++ OOP/ObjectOrientede/CodeTesting/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+#pragma warning disable
+
+namespace CodeTesting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
