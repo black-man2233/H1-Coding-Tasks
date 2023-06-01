5824e62593795dbce7c9a267e0da65e29ec4f281
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-06-01 09:08:20 +0200

    Trying out interfaces

--- OOP/ObjectOrientede/CodeTesting/Program.cs
+++ OOP/ObjectOrientede/CodeTesting/Program.cs
@@ -8,6 +8,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program program = new Program();
+        program.AnimalSpeak(new Cat());
+    }
+
+    void AnimalSpeak(IAnimal animal)
+    {
+        Console.WriteLine(animal.Speak());
+    }
+}
+
+public interface IAnimal
+{
+    string Name { get; set; }
+    string Speak();
+}
+
+public class Dog : IAnimal
+{
+    public string Name { get; set; }
+
+    public string Speak()
+    {
+        return "Woof";
+    }
+
+    public Dog()
+    {
+        Name = "Dog";
+    }
+}
+
+class Cat : IAnimal
+{
+    public string Name { get; set; }
+
+    public string Speak()
+    {
+        return "Meow";
+    }
+
+    public Cat()
+    {
+        Name = "Cat";
     }
 }
